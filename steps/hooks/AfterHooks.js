82fe554f8507b69a4c7056366efe744b4fc5339e
Матвей Мурашко <stranger2626@gmail.com>
2020-10-17 09:46:51 +0300

    added after hook for taking screenshots

---
+++ steps/hooks/AfterHooks.js
@@ -0,0 +1,10 @@
+const { After, Status } = require("cucumber");
+const { browser } = require("protractor");
+
+After(async function (testCase) {
+	if (testCase.result.status === Status.FAILED) {
+    const screenShot = await browser.takeScreenshot()
+    const decodedImage = new Buffer.from(screenShot, `base64`);
+    return this.attach(decodedImage, `image/png`);
+	}
+});
