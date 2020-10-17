17caa0fec31ac38e9eaf1ea630ddb3e5b550d7c9
Patsyna, Dzianis <dzianis.patsyna@epicgames.com>
2020-10-17 10:34:48 +0300

    Add page object for country statistic

---
+++ po/pages/StateDetails.js
@@ -0,0 +1,18 @@
+const BasePage = require("./BasePage");
+
+class StateDetails extends BasePage {
+
+    constructor() {
+        super();
+
+        this.defineElement({alias: "State Name Title", selector: ".state-name"});
+        // Statistics Label`s Amounts
+        this.defineElement({alias: "Confirmed Amount", selector: ".Level .is-confirmed h1"});
+        this.defineElement({alias: "Active Amount", selector: ".Level .is-confirmed h1"});
+        this.defineElement({alias: "Recovered Amount", selector: ".Level .is-recovered h1"});
+        this.defineElement({alias: "Deceased Amount", selector: ".Level .is-deceased h1"});
+        this.defineElement({alias: "Total Tested", selector: ".header-right h2"});
+    }
+}
+
+module.exports = StateDetails;
