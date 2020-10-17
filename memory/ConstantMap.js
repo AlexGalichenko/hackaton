a5ac4434e18015753b6db27d9d1ca752033f7122
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2020-10-17 08:50:25 +0300

    fixed constant map name

--- memory/ConstantMap.js
+++ memory/ConstantMap.js
@@ -1,10 +1,10 @@
 const { ConstantMap } = require("@cucumber-e2e/memory");
 
-class CCPConstantMap extends ConstantMap {
+class CovidIndiaConstantMap extends ConstantMap {
     constructor() {
         super();
         this.defineConstant("APP_URL", "https://www.covid19india.org/");
     }
 }
 
-module.exports = CCPConstantMap;
+module.exports = CovidIndiaConstantMap;
